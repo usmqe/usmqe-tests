070a2038b5b55c8a9f1e1fae7ad9fd42e828ac86
Martin Bukatovic <mbukatov@redhat.com>
2016-10-25 13:46:54 +0200

    initial top level conftest.py file

---
+++ conftest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+"""
+Top level conftest.py file of usmqe tests project.
+"""
+
+
+import pytest
+
+
+# list custom usmqe plugins here
+pytest_plugins = (
+    # "plugin.foobarbaz", # register plugin from ``plugin/foobarbaz.py`` file.
+    )
