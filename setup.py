0d0900773cf244bc01414baa6b6551987312e8d7
Martin Bukatovic <mbukatov@redhat.com>
2016-11-03 16:27:10 +0100

    add import all unit tests

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # setup.py file for tox to be able to run unit tests
 
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='usmqe-tests',
     license='GNU GPL v3.0',
-    packages=['usmqe'],
+    packages=find_packages(exclude=['doc', 'usmqe_tests', 'conf']),
     )
