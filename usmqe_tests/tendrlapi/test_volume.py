655edf523a44e58ab99c7bdbecec2c4c376836e5
Filip Balák <fbalak@redhat.com>
2016-11-23 10:19:32 -0500

    update test

--- usmqe_tests/tendrlapi/test_volume.py
+++ usmqe_tests/tendrlapi/test_volume.py
@@ -10,7 +10,7 @@ import re
 from usmqe.api.tendrlapi import tendrlapi
 from usmqe.gluster import gluster
 
-
+volume_id = ""
 LOGGER = pytest.get_logger('volume_test', module=True)
 """@pylatest default
 Setup
@@ -58,6 +58,7 @@ def test_cluster_import():
     		Return code should be **200** with data ``{"message": "OK"}``.
 
     	"""
+    global volume_id
     api = tendrlapi.ApiCommon()
     nodes = api.call(pattern="/1.0/GetNodeList")
     """@pylatest api/gluster.cluster_import
@@ -78,11 +79,14 @@ def test_cluster_import():
     	"""
     test_gluster = gluster.GlusterCommon()
     test_gluster.run_on_node(command="volume info")
-    post_data = '''{
-    "Node[]":["1b5a0ec3-d660-48cb-bc47-897789a5bc6", "32c96426-9cf5-41b9-84ce-a769c2a1a6f8", "887f9263-414e-480a-8026-4e14b48cfa6a", "007d2e7a-8717-488d-8adb-ffcf58fdab01"],
-    "Tendrl_context.sds_name": "gluster",
-    "Tendrl_context.sds_version": "3.8.3"
-    	 }'''
+    post_data = {
+        "Node[]":["1b5a0ec3-d660-48cb-bc47-897789a5bc6",
+            "32c96426-9cf5-41b9-84ce-a769c2a1a6f8",
+            "887f9263-414e-480a-8026-4e14b48cfa6a",
+            "007d2e7a-8717-488d-8adb-ffcf58fdab01"],
+        "Tendrl_context.sds_name": "gluster",
+        "Tendrl_context.sds_version": "3.8.3"
+        }
     #print( json.loads(gluster.actions(1)).items())
     #pytest.check( json.loads(gluster.actions(1)).keys() == json.loads(test_string).keys())
     #pytest.check( test.actions(1) == json.loads(expected_response))
@@ -113,69 +117,120 @@ def test_create_volume():
 
     	.. test_step:: 1
 
-    		Connect to Tendrl API via POST request to ``APIURL/cluster/:cluster_id/volume/attributes``
-    		Where cluster_id is set to 1.
+    		Connect to Tendrl API via POST request to ``APIURL/:cluster_id/GlusterCreateVolume``
+    		Where cluster_id is set to predefined value.
 
     	.. test_result:: 1
 
     		Server should return response in JSON format:
 
-    			{
-    			  "volname":{
-    				"type":"String"
-    			  },
-    			  "stripe_count":{
-    				"type":"Integer"
-    			  },
-    			  "replica_count":{
-    				"type":"Integer"
-    			  },
-    			  "disperse_count":{
-    				"type":"Integer"
-    			  },
-    			  "redundancy_count":{
-    				"type":"Integer"
-    			  },
-    			  "transport":{
-    				"type":"String"
-    			  },
-    			  "brickdetails":{
-    				"type":"list[brick]"
-    			  },
-    			  "force":{
-    				"type":"Boolean"
-    			  },
-    			  "directory":{
-    				"type":"String"
-    			  },
-    			  "bitrot":{
-    				"type":"Boolean"
-    			  },
-    			  "scrub-frequency":{
-    				"type":"Integer"
-    			  },
-    			  "scrub-throttle":{
-    				"type":"Integer"
-    			  }
-    			}
-
-    		Return code should be **200** with data ``{"message": "OK"}``.
+    		Return code should be **202** with data ``{"message": "Accepted"}``.
     		"""
+    global volume_id
     cluster_id = "147cac1f-fc1f-4ffb-9bff-97ccc95cdcf7"
     api = tendrlapi.ApiCommon()
-    post_data = '''{
-"Volume.volname":"Vol_test",
-"Volume.bricks":["dhcp-126-104.lab.eng.brq.redhat.com:/mnt/gluster2","dhcp-126-107.lab.eng.brq.redhat.com:/mnt/gluster2"]
-}'''
-    response = api.call(pattern="/1.0/{}/GlusterCreateVolume".format(cluster_id), method="POST", json=json.loads(post_data))
+    post_data = {
+        "Volume.volname":"Vol_test",
+        "Volume.bricks":["dhcp-126-104.lab.eng.brq.redhat.com:/mnt/gluster2","dhcp-126-107.lab.eng.brq.redhat.com:/mnt/gluster2"]
+    }
+    response = api.call(pattern="/1.0/{}/GlusterCreateVolume".format(cluster_id), method="POST", json=json.loads(json.dumps(post_data)))
 
     expected_response = 202
+    LOGGER.debug("post_data: %s" % json.dumps(post_data))
     LOGGER.debug("response: %s" % response.status_code)
     pytest.check( response.status_code == expected_response)
 
+    """@pylatest api/gluster.create_volume
+    	API-gluster: create_volume
+    	******************************
+
+    	.. test_metadata:: author fbalak@redhat.com
+
+    	Description
+    	===========
+
+    	Check if there is created volume on gluster nodes via CLI.
+
+    	.. test_step:: 1
+
+    		Connect to gluster node machine via ssh and run
+            ``gluster volume info command``
+
+    	.. test_result:: 1
+
+            There should be listed gluster volume named ``Vol_test``.
+
+    		"""
+    test_gluster = gluster.GlusterCommon()
+    xml = test_gluster.run_on_node(command="volume info")
+    vol_name = xml.findtext(".//name")
+    LOGGER.debug("res: %s" % vol_name)
+
+    volume_id = xml.findtext(".//id")
+    expected_vol_name = "Vol_test"
+    pytest.check( vol_name == expected_vol_name)
+
+def test_delete_volume():
+    """@pylatest api/gluster.delete_volume
+    	API-gluster: delete_volume
+    	******************************
+
+    	.. test_metadata:: author fbalak@redhat.com
+
+    	Description
+    	===========
+
+        Delete gluster volume ``Vol_test`` via API.
+
+    	.. test_step:: 1
+
+    		Connect to Tendrl API via POST request to ``APIURL/:cluster_id/GlusterDeleteVolume``
+    		Where cluster_id is set to predefined value.
+
+    	.. test_result:: 1
+
+    		Server should return response in JSON format:
+
+    		Return code should be **202** with data ``{"message": "Accepted"}``.
+    		"""
+    global volume_id
+    cluster_id = "147cac1f-fc1f-4ffb-9bff-97ccc95cdcf7"
+    api = tendrlapi.ApiCommon()
+    post_data = {
+            "Volume.volname":"Vol_test",
+            "Volume.vol_id":volume_id
+            }
+    response = api.call(pattern="/1.0/{}/GlusterDeleteVolume".format(cluster_id), method="POST", json=json.loads(json.dumps(post_data)))
+
+    expected_response = 202
+    LOGGER.debug("post_data: %s" % post_data)
+    LOGGER.debug("response: %s" % response.status_code)
+    pytest.check( response.status_code == expected_response)
+
+    """@pylatest api/gluster.create_volume
+    	API-gluster: create_volume
+    	******************************
+
+    	.. test_metadata:: author fbalak@redhat.com
+
+    	Description
+    	===========
+
+    	Check if there is created volume on gluster nodes via CLI.
+
+    	.. test_step:: 1
+
+    		Connect to gluster node machine via ssh and run
+            ``gluster volume info command``
+
+    	.. test_result:: 1
+
+            There should be listed gluster volume named ``Vol_test``.
+
+    		"""
     test_gluster = gluster.GlusterCommon()
     vol_name = test_gluster.run_on_node(command="volume info").findtext(".//name")
     LOGGER.debug("res: %s" % vol_name)
 
     expected_vol_name = "Vol_test"
-    pytest.check( vol_name == expected_vol_name)
+    pytest.check( vol_name != expected_vol_name)
