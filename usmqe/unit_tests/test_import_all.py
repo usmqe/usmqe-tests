a040611ff1b929d10009d832f2f7fcba1c759db3
ltrilety <ltrilety@redhat.com>
2017-02-08 12:48:12 +0100

    fix flake issue

--- usmqe/unit_tests/test_import_all.py
+++ usmqe/unit_tests/test_import_all.py
@@ -16,7 +16,8 @@ def list_submodules(module_path, module_prefix):
     For given module, return list of full module path names for all submodules
     recursively.
     """
-    return [name for _, name, _ in pkgutil.walk_packages(path=[module_path], prefix=module_prefix)]
+    return [name for _, name, _ in
+            pkgutil.walk_packages(path=[module_path], prefix=module_prefix)]
 
 
 # parametrize makes this case run for every submodule in webstr module
@@ -29,5 +30,5 @@ def test_import(module):
         importlib.import_module(module)
     # TODO: FIXME Remove try except block when webstr will be public
     except ImportError as exc:
-        if not 'webstr' in str(exc):
+        if 'webstr' not in str(exc):
             raise exc
