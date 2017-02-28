db3b43794ef312d28be8812e01da9654b8df3bab
ltrilety <ltrilety@redhat.com>
2017-02-28 14:24:11 +0100

    Update Hosts page model

--- usmqe/web/tendrl/mainpage/hosts/pages.py
+++ usmqe/web/tendrl/mainpage/hosts/pages.py
@@ -30,6 +30,25 @@ class HostsItem(contentviews.ListViewRow):
         '_root',
         'status_icon',
         'name_label',
+        # TODO uncomment following lines when performance monitoring
+        #      will work and charts will be available
+        # 'storage_label',
+        # 'storage_used_chart',
+        # 'storage_used_nr',
+        # 'storage_total_nr',
+        # 'cpu_label',
+        # 'cpu_percent_chart',
+        # 'cpu_percent',
+        # 'memory_label',
+        # 'memory_used_chart',
+        # 'memory_used_nr',
+        # 'memory_total_nr',
+        'cluster_label',
+        'cluster_value',
+        'roles_label',
+        'roles_value',
+        'alerts_label',
+        'alerts_value',
         'menu_link']
 
     @property
@@ -41,6 +60,49 @@ class HostsItem(contentviews.ListViewRow):
             status_icon element title
         """
         return self._model.status_icon.value
+
+    @property
+    def name(self):
+        """
+        returns proper hostname
+        """
+        return self._model.name.text
+
+    @property
+    def ip(self):
+        """
+        returns host ip address
+        """
+        return self._model.ip.text
+
+    @property
+    def cluster(self):
+        """
+        returns cluster in which host is located
+        """
+        return self._model.cluster_value.text
+
+    @property
+    def role(self):
+        """
+        return host role
+            text value of roles_value field
+        """
+        return self._model.roles_value.text
+
+    @property
+    def alerts_nr(self):
+        """
+        returns number of alerts related to the cluster as string
+            text value of alerts_value field
+        """
+        return self._model.alerts_value.text
+
+    def click_on_alerts(self):
+        """
+        click on alerts number
+        """
+        self._model.alerts_value.click()
 
     def open_menu(self):
         """
