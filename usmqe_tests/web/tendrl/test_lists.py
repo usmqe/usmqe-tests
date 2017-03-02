f60a7612a5c314bcc2e9362832c8280799c5e411
ltrilety <ltrilety@redhat.com>
2017-03-02 15:25:09 +0100

    add roles check to the hosts list tests

--- usmqe_tests/web/tendrl/test_lists.py
+++ usmqe_tests/web/tendrl/test_lists.py
@@ -21,6 +21,9 @@ def test_hosts_list(log_in, testcase_end):
     navMenuBar = log_in.init_object
     hosts_list = navMenuBar.open_hosts()
     inventory_hosts = usmqe.inventory.role2hosts('usm_nodes')
+    gluster_hosts = usmqe.inventory.role2hosts('gluster')
+    ceph_mon_hosts = usmqe.inventory.role2hosts('ceph_mon')
+    ceph_osd_hosts = usmqe.inventory.role2hosts('ceph_osd')
     pytest.check(
         len(hosts_list) == len(inventory_hosts),
         'There should be exactly {} hosts'.format(len(inventory_hosts)))
@@ -30,9 +33,22 @@ def test_hosts_list(log_in, testcase_end):
         pytest.check(host.is_present,
                      'Any host (line in Hosts list) should have some elements')
         host_name = host.name
+        host_role = host.role
         pytest.check(host_name in inventory_hosts,
                      'host {} present in the list'.format(host_name))
         del(inventory_hosts[inventory_hosts.index(host_name)])
+        if host_name in gluster_hosts:
+            pytest.check(host_role == 'Peer',
+                         "Gluster host should have a 'Peer' role"
+                         ", it has '{}'".format(host_role))
+        elif host_name in ceph_osd_hosts:
+            pytest.check('OSD Host' in host_role,
+                         "host should have OSD role"
+                         ", it has '{}'".format(host_role))
+        elif host_name in ceph_mon_hosts:
+            pytest.check('Monitor' in host_role,
+                         "host should have Monitor role"
+                         ", it has '{}'".format(host_role))
 
 
 @pytest.mark.parametrize("sds_name", ["gluster"])
