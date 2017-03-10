714a4fa424b34fbda12f0ddba85848a2e81a44ae
ltrilety <ltrilety@redhat.com>
2017-03-10 15:11:44 +0100

    correct get_name method docstring

--- usmqe_tests/conftest.py
+++ usmqe_tests/conftest.py
@@ -9,10 +9,14 @@ pytest.set_logger(LOGGER)
 def get_name(fname):
     """
     Generate test name from method name.
+
+    Parameters:
+        fname (string): function name with parametrized arguments
+                        e.g. "Test <Function 'test_my_method[1-my_arg-42]'>"
     """
-    # get the node name, function with parameters
+    # take only function name with its parameters
     import re
-    fname = re.sub(".*'(?P<name>.*)'.*", '\g<name>', str(fname))
+    fname = re.sub(".*'(?P<name>.*)'.*", '\g<name>', fname)
     # remove 'test_' from the beginning and
     # replace all underscores with spaces
     return fname[5:].replace('_', ' ')
@@ -32,6 +36,6 @@ def logger_testcase(request):
     """
     Mark start and end of a test case using usmqe logger.
     """
-    LOGGER.testStart(get_name(request.node))
+    LOGGER.testStart(get_name(str(request.node)))
     yield
     LOGGER.testEnd()
