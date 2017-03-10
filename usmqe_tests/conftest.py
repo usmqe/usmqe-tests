caf2f7f9aafc2abe85e64316ec67d31c8e13b037
ltrilety <ltrilety@redhat.com>
2017-03-10 15:02:12 +0100

    Add parameters to the test name

--- usmqe_tests/conftest.py
+++ usmqe_tests/conftest.py
@@ -1,15 +1,21 @@
 import pytest
+from itertools import cycle
 
 
 # initialize usmqe logging module
 LOGGER = pytest.get_logger("pytests_test")
 pytest.set_logger(LOGGER)
 
+parametrized_tests = {}
+
 
 def get_name(fname):
     """
     Generate test name from method name.
     """
+    # get the node name, function with parameters
+    import re
+    fname = re.sub(".*'(?P<name>.*)'.*", '\g<name>', str(fname))
     # remove 'test_' from the beginning and
     # replace all underscores with spaces
     return fname[5:].replace('_', ' ')
@@ -29,6 +35,6 @@ def logger_testcase(request):
     """
     Mark start and end of a test case using usmqe logger.
     """
-    LOGGER.testStart(get_name(request.function.__name__))
+    LOGGER.testStart(get_name(request.node))
     yield
     LOGGER.testEnd()
