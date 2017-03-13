c40a9846cc62f97f3318e926eb42840bfc3eb4af
ltrilety <ltrilety@redhat.com>
2017-03-13 12:46:02 +0100

    Add txt logging

--- plugin/log_assert.py
+++ plugin/log_assert.py
@@ -192,6 +192,8 @@ def pytest_runtest_makereport(item, call):
 def get_logger(*args, **kwargs):
     if 'verbose_lvl' not in kwargs:
         kwargs['verbose_lvl'] = 0
+    if 'output' not in kwargs:
+        kwargs['output'] = "std, txt"
     logger = mrglog.get_logger(*args, **kwargs)
     for handler in logger.handlers:
         # logging.FileHandler is probably inherited from logging.StreamHandler
