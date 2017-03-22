90f75b3df14f8bdf512cf8143b1c9dea6719c3dd
Martin Bukatovic <mbukatov@redhat.com>
2017-03-22 19:10:05 +0100

    test_yum_install with "rpm chroot" fixture
    This test check that rpm packages can be installed (and removed) using
    yum on clean CentOS 7 machine.

--- usmqe_tests/rpm/conftest.py
+++ usmqe_tests/rpm/conftest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 
+import glob
 import os
 import tempfile
 import textwrap
@@ -7,6 +8,74 @@ import textwrap
 import pytest
 import requests
 import subprocess
+
+
+@pytest.fixture(scope="module")
+def chroot_dir(rpm_repo):
+    """
+    Create chroot enviroment for rpm installation test.
+    """
+    gpgkey_url = pytest.config.getini("usm_rpm_gpgkey_url")
+    tmpdirname = tempfile.mkdtemp()
+    # create minimal directory tree
+    os.makedirs(os.path.join(tmpdirname, "var/lib/rpm"))
+    os.makedirs(os.path.join(tmpdirname, "etc/yum.repos.d"))
+    os.makedirs(os.path.join(tmpdirname, "tmp"))
+    # download repo gpg key of the product we are testing
+    req = requests.get(gpgkey_url)
+    assert req.status_code == 200
+    gpgkey_file = os.path.join(tmpdirname, "tmp", "pubkey.gpg")
+    with open(gpgkey_file, "w") as keyfile:
+        keyfile.write(req.content.decode())
+    # create repo file of the product we are testing
+    repofile_path = os.path.join(tmpdirname, "etc/yum.repos.d/tmp.repo")
+    repofile_template = textwrap.dedent("""
+    [tmp]
+    name=Temporary Yum Repository
+    baseurl={}
+    enabled=1
+    gpgkey={}
+    gpgcheck=1
+    """)
+    with open(repofile_path, "w") as repofile:
+        repofile.write(repofile_template.format(rpm_repo, gpgkey_url))
+    # initialize rpmdb
+    rpm_cmd = ["rpm", "--root", tmpdirname, "--initdb"]
+    subprocess.run(rpm_cmd, cwd=os.path.join(tmpdirname), check=True)
+    # download and install centos-release and epel-release packages
+    # in the tmpdirname using default system repositories (we expect to run
+    # on CentOS anyway)
+    for rpm_name in ("centos-release", "epel-release"):
+        yum_cmd = [
+            "yumdownloader",
+            "--destdir={}".format(os.path.join(tmpdirname, "tmp")),
+            rpm_name]
+        subprocess.run(yum_cmd, cwd=tmpdirname, check=True)
+        files = glob.glob(os.path.join(tmpdirname, "tmp", rpm_name + "*.rpm"))
+        assert len(files) == 1
+        rpm_path = files[0]
+        # note: we expect that current user can do run anything as root via sudo
+        # (which is configured in usmqe-setup playbook for qe server role)
+        rpm_cmd = [
+            "sudo",
+            "rpm", "--root", tmpdirname, "-iv", "--nodeps", rpm_path]
+        subprocess.run(rpm_cmd, cwd=tmpdirname, check=True)
+    # import the gpg keys for all yum repositories
+    repo_keys = [
+        os.path.join(tmpdirname, "etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7"),
+        os.path.join(tmpdirname, "etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7"),
+        os.path.join(tmpdirname, "tmp/pubkey.gpg"), # product gpg repo key
+        ]
+    for key in repo_keys:
+        cmd = ["rpm", "--root", tmpdirname, "--import", key]
+        subprocess.run(cmd, cwd=os.path.join(tmpdirname), check=True)
+    # and that's all
+    yield tmpdirname
+    # teardown:
+    # since most files in tmpdirname are owned by root now, let's do the
+    # cleanup manually
+    cmd = ["sudo", "rm", tmpdirname, "-rf"]
+    subprocess.run(cmd, cwd=tmpdirname, check=True)
 
 
 @pytest.fixture(scope="module")
@@ -59,6 +128,8 @@ def rpm_package(request, rpm_repo):
     Fixture downloads given rpm package from given repository
     and returns it's local path to the test. Downloaded package
     is deleted later during tear down phase.
+
+    GnuPG signature is not verified during package download.
     """
     with tempfile.TemporaryDirectory() as tmpdirname:
         # name of rpm package under test
---
+++ usmqe_tests/rpm/test_install.py
@@ -0,0 +1,52 @@
+# -*- coding: utf8 -*-
+
+import pytest
+import subprocess
+import tempfile
+
+
+LOGGER = pytest.get_logger(__name__, module=True)
+
+
+@pytest.mark.parametrize("rpm_name", [
+    "tendrl-commons",
+    "tendrl-node-agent",
+    "tendrl-ceph-integration",
+    "tendrl-gluster-integration",
+    "tendrl-api",
+    "tendrl-dashboard",
+    "tendrl-node-monitoring",
+    "tendrl-performance-monitoring",
+    ])
+def test_yum_install(chroot_dir, rpm_name):
+    """
+    Try to install and uninstall rpm package via yum in CentOS 7 chroot.
+    """
+    cmd = [
+        "sudo",
+        "yum", "--installroot=%s" % chroot_dir, "-y", "install", rpm_name]
+    # running yum install
+    LOGGER.info(" ".join(cmd))
+    cp = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    LOGGER.debug("STDOUT: %s", cp.stdout)
+    LOGGER.debug("STDERR: %s", cp.stderr)
+    check_msg = "return code of 'yum install {}' should be 0 indicating no errors"
+    pytest.check(cp.returncode == 0, msg=check_msg.format(rpm_name))
+    # check after installation
+    cmd = ["rpm", "-q", rpm_name, "--root", chroot_dir]
+    LOGGER.info(" ".join(cmd))
+    cp = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    LOGGER.debug("STDOUT: %s", cp.stdout)
+    LOGGER.debug("STDERR: %s", cp.stderr)
+    check_msg = "return code of 'rpm -q {}' should be 0 indicating no errors"
+    pytest.check(cp.returncode == 0, msg=check_msg.format(rpm_name))
+    # running yum remove
+    cmd = [
+        "sudo",
+        "yum", "--installroot=%s" % chroot_dir, "-y", "remove", rpm_name]
+    LOGGER.info(" ".join(cmd))
+    cp = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    LOGGER.debug("STDOUT: %s", cp.stdout)
+    LOGGER.debug("STDERR: %s", cp.stderr)
+    check_msg = "return code of 'yum remove {}' should be 0 indicating no errors"
+    pytest.check(cp.returncode == 0, msg=check_msg.format(rpm_name))
