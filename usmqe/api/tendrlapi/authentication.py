daf09139a1bd5409edb18569e52b5ea5cda8ba07
Filip Balák <fbalak@redhat.com>
2017-03-24 06:56:52 -0400

    fix authentication

--- usmqe/api/tendrlapi/authentication.py
+++ usmqe/api/tendrlapi/authentication.py
@@ -6,6 +6,8 @@ import requests
 
 import pytest
 
+import json
+
 from usmqe.api.tendrlapi.common import TendrlApi
 
 LOGGER = pytest.get_logger("tendrlapi.authentication", module=True)
@@ -31,12 +33,14 @@ class Authentication(TendrlApi):
             asserts_in: assert values for this call and this method
         """
         pattern = "login"
+        post_data = {"username": username, "password": password}
         request = requests.post(
-            pytest.config.getini("usm_api_url") + pattern)
+            pytest.config.getini("usm_api_url") + pattern, data=json.dumps(post_data))
         self.print_req_info(request)
         self.check_response(request, asserts_in)
         self.username = username
         self.role = "admin"
+        LOGGER.debug("access_token:  {}".format(request.json()))
         self.access_token = request.json()["access_token"]
 
     def logout(self, asserts_in=None):
