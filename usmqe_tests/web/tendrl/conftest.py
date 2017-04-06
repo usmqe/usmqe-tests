e7da8045c18859384975d42b54a092c12fa34bfe
ltrilety <ltrilety@redhat.com>
2017-04-06 17:37:04 +0200

    Add log-in page support

--- usmqe_tests/web/tendrl/conftest.py
+++ usmqe_tests/web/tendrl/conftest.py
@@ -7,8 +7,7 @@ Author: ltrilety
 
 import pytest
 
-# Coming soon ...
-# from usmqe.web.tendrl.loginpage import pages as loginpage
+from usmqe.web.tendrl.loginpage import pages as loginpage
 from usmqe.web.tendrl.mainpage.landing_page.pages import get_landing_page
 
 
@@ -19,11 +18,12 @@ def log_in(testcase_set):
     that the default user is already logged in
     for the test to work should extend this class.
     """
-# NOTE: For now it just solves the landing page - no log in
-#       Remove following line when log in is done
-# TODO
-# https://github.com/Tendrl/specifications/issues/128
-    testcase_set.driver.get(pytest.config.getini("usm_web_url"))
+    testcase_set.loginpage = loginpage.LoginPage(
+        testcase_set.driver,
+        pytest.config.getini("usm_web_url"))
+    testcase_set.loginpage.login_user(
+        pytest.config.getini("usm_username"),
+        pytest.config.getini("usm_password"))
     testcase_set.init_object = get_landing_page(testcase_set.driver)
     msg = "Initial element - Home page or navigation part of the main page "\
         "should contain all required components."
