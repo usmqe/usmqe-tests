7ade855eded7d0a4f6c1f883ee8602db083a12f5
ltrilety <ltrilety@redhat.com>
2017-06-13 22:11:08 +0200

    Alter import tests
        changing tests for better output

--- usmqe_tests/web/ceph/test_import_ceph_cluster.py
+++ usmqe_tests/web/ceph/test_import_ceph_cluster.py
@@ -9,30 +9,32 @@ import pytest
 from usmqe.web.tendrl import cluster_work
 
 
-@pytest.mark.other
-def test_initial_import_cluster(valid_credentials):
+@pytest.mark.ceph
+def test_initial_import_ceph_cluster(valid_credentials):
     """
-    positive import cluster test
+    positive import ceph cluster test
 
     NOTE: 1. No cluster has to be imported
-          2. There has to be at least one cluster which could be imported
+          2. There has to be at least one ceph cluster which could be imported
     """
 # TODO: Choose specific cluster
     cluster_work.initial_import_cluster(
         valid_credentials.driver,
         valid_credentials.init_object,
-        valid_credentials.loginpage)
+        valid_credentials.loginpage,
+        cluster_type='ceph')
 
 
-@pytest.mark.other
-def test_import_cluster(valid_credentials):
+@pytest.mark.ceph
+def test_import_ceph_cluster(valid_credentials):
     """
-    positive import cluster test
+    positive import ceph cluster test
 
     NOTE: 1. Some cluster has to be already present in the list
-          2. There has to be at least one cluster which could be imported
+          2. There has to be at least one ceph cluster which could be imported
     """
 # TODO: Choose specific cluster
     cluster_work.import_cluster(
         valid_credentials.driver,
-        valid_credentials.init_object)
+        valid_credentials.init_object,
+        cluster_type='ceph')
---
+++ usmqe_tests/web/gluster/test_import_gluster_cluster.py
@@ -0,0 +1,40 @@
+"""
+Description: Simple import cluster tests
+
+Author: ltrilety
+"""
+
+import pytest
+
+from usmqe.web.tendrl import cluster_work
+
+
+@pytest.mark.gluster
+def test_initial_import_gluster_cluster(valid_credentials):
+    """
+    positive import gluster cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one gluster cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    cluster_work.initial_import_cluster(
+        valid_credentials.driver,
+        valid_credentials.init_object,
+        valid_credentials.loginpage,
+        cluster_type='gluster')
+
+
+@pytest.mark.gluster
+def test_import_gluster_cluster(valid_credentials):
+    """
+    positive import gluster cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one gluster cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    cluster_work.import_cluster(
+        valid_credentials.driver,
+        valid_credentials.init_object,
+        cluster_type='gluster')
