91c953792d0d5d46f3f821c98d7bd51263b8b03b
ltrilety <ltrilety@redhat.com>
2017-06-13 19:37:27 +0200

    update import cluster tests
        add ceph and gluster specific tests, use tags

--- usmqe_tests/web/others/test_import_cluster.py
+++ usmqe_tests/web/others/test_import_cluster.py
@@ -6,15 +6,18 @@ Author: ltrilety
 
 import pytest
 
-from usmqe.web.tendrl.import_cluster import import_cluster
 from usmqe.web.tendrl.mainpage.navpage.pages import NavMenuBars
 from usmqe.web.tendrl.mainpage.clusters.cluster_list.pages import\
     ClustersList, ClustersMenu
 from usmqe.web.tendrl.landing_page.pages import get_landing_page
 from usmqe.web.tendrl.auxiliary.pages import UpperMenu
+from usmqe.web.tendrl.mainpage.clusters.import_cluster_wizard.pages\
+    import ImportCluster
+from usmqe.web.tendrl import cluster_work
 
 
-def test_initial_import_cluster(valid_credentials):
+@pytest.fixture
+def initial_import_cluster(valid_credentials):
     """
     positive import cluster test
 
@@ -27,8 +30,11 @@ def test_initial_import_cluster(valid_credentials):
                  ' if there is no cluster present',
                  hard=True)
 
-# TODO: Import specific cluster
-    cluster_ident = import_cluster(valid_credentials.driver, home_page)
+    yield valid_credentials
+
+    import_page = ImportCluster(valid_credentials.driver)
+    cluster_ident = cluster_work.import_cluster(valid_credentials.driver,
+                                                import_page)
 
     # log out and log in again
     upper_menu = UpperMenu(valid_credentials.driver)
@@ -59,7 +65,8 @@ def test_initial_import_cluster(valid_credentials):
                  'The imported cluster should be present in the cluster list')
 
 
-def test_import_cluster(valid_credentials):
+@pytest.fixture
+def import_cluster(valid_credentials):
     """
     positive import cluster test
 
@@ -72,7 +79,214 @@ def test_import_cluster(valid_credentials):
                  hard=True)
     clusters_list = nav_page.open_clusters()
     clusters_nr = len(clusters_list)
-    clusters_menu = ClustersMenu(valid_credentials.driver)
+    valid_credentials.init_object = ClustersMenu(valid_credentials.driver)
 
-# TODO: Import specific cluster
-    import_cluster(valid_credentials.driver, clusters_menu, clusters_nr)
+    yield valid_credentials
+
+    import_page = ImportCluster(valid_credentials.driver)
+    import_cluster(valid_credentials.driver, import_page, clusters_nr)
+
+
+def aux_import_gluster_cluster(driver, import_init_page):
+    """
+    auxiliary function for import gluster cluster
+    choose first available gluster cluster
+
+    Parameters:
+        driver: selenium driver
+        import_init_page: WebstrPage instance of page where import
+                          button is present
+    """
+    initial_import_cluster.init_object.start_import_cluster()
+
+    # Choose which cluster should be imported
+    # Select first gluster cluster in the list of available clusters
+    import_page = ImportCluster(driver)
+    for cluster in import_page.avail_clusters:
+        cluster.click()
+        release = import_page.storage_service.lower()
+        if 'gluster' in release:
+            break
+    pytest.check('gluster' in release,
+                 'There should be some gluster cluster available',
+                 hard=True)
+
+
+def aux_import_ceph_cluster(driver, import_init_page):
+    """
+    auxiliary function for import ceph cluster
+    choose first available ceph cluster
+
+    Parameters:
+        driver: selenium driver
+        import_init_page: WebstrPage instance of page where import
+                          button is present
+    """
+    initial_import_cluster.init_object.start_import_cluster()
+
+    # Choose which cluster should be imported
+    # Select first ceph cluster in the list of available clusters
+    import_page = ImportCluster(driver)
+    for cluster in import_page.avail_clusters:
+        cluster.click()
+        release = import_page.storage_service.lower()
+        if 'ceph' in release:
+            break
+    pytest.check('ceph' in release,
+                 'There should be some ceph cluster available',
+                 hard=True)
+
+
+def aux_import_generic_cluster(driver, import_init_page):
+    """
+    auxiliary function for import cluster
+    choose first available cluster
+
+    Parameters:
+        driver: selenium driver
+        import_init_page: WebstrPage instance of page where import
+                          button is present
+    """
+    import_init_page.start_import_cluster()
+    import_page = ImportCluster(driver)
+    pytest.check(len(import_page.avail_clusters) > 0,
+                 'There should be some cluster available',
+                 hard=True)
+
+
+@pytest.fixture
+def initial_import_gluster_cluster(initial_import_cluster):
+    """
+    positive import gluster cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one gluster cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    aux_import_gluster_cluster(initial_import_cluster.driver,
+                           initial_import_cluster.init_object)
+
+
+@pytest.fixture
+def initial_import_ceph_cluster(initial_import_cluster):
+    """
+    positive import ceph cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one ceph cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    aux_import_ceph_cluster(initial_import_cluster.driver,
+                        initial_import_cluster.init_object)
+
+
+@pytest.fixture
+def initial_import_generic_cluster(initial_import_cluster):
+    """
+    positive import cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    aux_import_generic_cluster(initial_import_cluster.driver,
+                           initial_import_cluster.init_object)
+
+
+@pytest.fixture
+def import_gluster_cluster(import_cluster):
+    """
+    positive import gluster cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one gluster cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    aux_import_gluster_cluster(import_cluster.driver,
+                           import_cluster.init_object)
+
+
+@pytest.fixture
+def import_ceph_cluster(import_cluster):
+    """
+    positive import ceph cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one ceph cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    aux_import_ceph_cluster(import_cluster.driver,
+                        import_cluster.init_object)
+
+
+@pytest.fixture
+def import_generic_cluster(import_cluster):
+    """
+    positive import cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one cluster which could be imported
+    """
+# TODO: Choose specific cluster
+    aux_import_generic_cluster(import_cluster.driver,
+                           import_cluster.init_object)
+
+
+@pytest.mark.Gluster
+def test_initial_import_gluster_cluster(initial_import_gluster_cluster):
+    """
+    positive import gluster cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one gluster cluster which could be imported
+    """
+
+
+@pytest.mark.Ceph
+def test_initial_import_ceph_cluster(initial_import_ceph_cluster):
+    """
+    positive import ceph cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one ceph cluster which could be imported
+    """
+
+
+@pytest.mark.Other
+def test_initial_import_generic_cluster(initial_import_generic_cluster):
+    """
+    positive import cluster test
+
+    NOTE: 1. No cluster has to be imported
+          2. There has to be at least one cluster which could be imported
+    """
+
+
+@pytest.mark.Gluster
+def test_import_gluster_cluster(import_gluster_cluster):
+    """
+    positive import gluster cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one gluster cluster which could be imported
+    """
+
+
+@pytest.mark.Ceph
+def test_import_ceph_cluster(import_ceph_cluster):
+    """
+    positive import ceph cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one ceph cluster which could be imported
+    """
+
+
+@pytest.mark.Other
+def test_import_generic_cluster(import_generic_cluster):
+    """
+    positive import cluster test
+
+    NOTE: 1. Some cluster has to be already present in the list
+          2. There has to be at least one cluster which could be imported
+    """
