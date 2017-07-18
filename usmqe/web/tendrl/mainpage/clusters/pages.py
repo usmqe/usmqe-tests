fae3be7146cde257246b3a4b14ba1ccd8649448c
ltrilety <ltrilety@redhat.com>
2017-07-18 17:30:16 +0200

    Fix check_hosts method
        the method now accepts a list of host names
        also role is not mandatory key in the dictionary any more

--- usmqe/web/tendrl/mainpage/clusters/pages.py
+++ usmqe/web/tendrl/mainpage/clusters/pages.py
@@ -22,36 +22,52 @@ def check_hosts(hosts_list, page_hosts_list):
     check if all hosts in host_list are present on the page (in page_host_list)
 
     Parameters:
-        hosts_list (list): list of dictionaries
-                          {'hostname': <hostname>, 'role': <role>, ...
+        hosts_list (list): list of hostnames
+                            or
+                           list of dictionaries
+                           {'hostname': <hostname>, 'role': <role>, ...
         page_hosts_list (list): list representing lines in the page hosts list
     """
     aux_list = copy.deepcopy(hosts_list)
     for host_row in page_hosts_list:
         found = False
-        for host in aux_list:
-            if host['hostname'] in host_row.name:
+        if aux_list and type(aux_list[0]) == 'dict':
+            for host in aux_list:
+                if host['hostname'] in host_row.name:
+                    found = True
+                    # check host role if possible
+                    if 'role' in host.keys():
+                        if type(host['role']) is list:
+                            for role in host['role']:
+                                pytest.check(
+                                    role in host_row.role,
+                                    "Host {} should have '{}' role "
+                                    "it has '{}'".format(
+                                        host_row.name, role, host_row.role))
+                        else:
+                            pytest.check(
+                                host['role'] == host_row.role,
+                                "Host {} should have '{}' role "
+                                "it has '{}'".format(
+                                    host_row.name, host['role'],
+                                    host_row.role))
+                    aux_list.remove(host)
+                    break
+        else:
+            if host_row.name in aux_list:
                 found = True
-                if type(host['role']) is list:
-                    for role in host['role']:
-                        pytest.check(
-                            role in host_row.role,
-                            "Host {} should have '{}' role it has '{}'".format(
-                                host_row.name, role, host_row.role))
-                else:
-                    pytest.check(
-                        host['role'] == host_row.role,
-                        "Host {} should have '{}' role it has '{}'".format(
-                            host_row.name, host['role'], host_row.role))
-                aux_list.remove(host)
-                break
+                aux_list.remove(host_row.name)
         pytest.check(
             found,
             'A host {} should be part of the hosts list'.format(host_row.name))
+    if aux_list and type(aux_list[0]) == 'dict':
+        hostnames = [host['hostname'] for host in aux_list]
+    else:
+        hostnames = aux_list
     pytest.check(
         aux_list == [],
         'All cluster hosts should be listed on page '
-        '(not listed: {})'.format([host['hostname'] for host in aux_list]))
+        '(not listed: {})'.format(hostnames))
 
 
 class ClustersWorkBase(object):
