b72d74fa2dbad03db6801708211842bb4f378b63
Filip Balák <fbalak@redhat.com>
2017-08-18 07:25:09 -0400

    update valid_devices fixture
    according to https://github.com/usmqe/usmqe-tests/pull/115

--- usmqe_tests/api/gluster/conftest.py
+++ usmqe_tests/api/gluster/conftest.py
@@ -96,21 +96,26 @@ def valid_devices(valid_session_credentials, count=1):
     """
 
     api = glusterapi.TendrlApiGluster(auth=valid_session_credentials)
-    device_info = [list(x["localstorage"]["blockdevices"]["all"].values())[0]
-                   if x["localstorage"]["blockdevices"]["all"] is not None
-                   else None for x in api.get_nodes()["nodes"]]
-    device_info = [x for x in device_info if x is not None]
-    devices = [x["device_kernel_name"] for x in device_info]
+    nodes_free_devs = {x["node_id"]: list(x["localstorage"]["blockdevices"]["free"].values())
+                       for x in api.get_nodes()["nodes"]
+                       if len(x["localstorage"]["blockdevices"]["free"]) > 0}
+    nodes_free_kern_name = {}
+    for node_id in nodes_free_devs:
+        for device in nodes_free_devs[node_id]:
+            if node_id not in nodes_free_kern_name:
+                nodes_free_kern_name[node_id] = []
+            nodes_free_kern_name[node_id].append(device["device_kernel_name"])
 
     try:
-        return devices[0:count]
-    except IndexError as err:
+        return {node_id: sorted(nodes_free_kern_name[node_id])[0:count]
+                for node_id in nodes_free_kern_name}
+    except IndexError as e:
         raise Exception(
                 "TypeError({0}): There are not enough devices. There are: {1}. {2}"
                 .format(
-                    err.errno,
-                    devices,
-                    err.strerror))
+                    e.errno,
+                    nodes_free_kern_name,
+                    e.strerror))
 
 
 @pytest.fixture
