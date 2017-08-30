77d0fdef09470620ce2c0cd981cdae9018d128c3
ltrilety <ltrilety@redhat.com>
2017-08-30 12:30:11 +0200

    Fix clusters list

--- usmqe/web/tendrl/mainpage/clusters/cluster_list/models.py
+++ usmqe/web/tendrl/mainpage/clusters/cluster_list/models.py
@@ -6,13 +6,12 @@ Common page models for clusters.
 from webstr.core import By, PageElement
 from webstr.common.form import models as form
 import webstr.patternfly.contentviews.models as contentviews
-import webstr.patternfly.dropdown.models as dropdown
 
 from usmqe.web.tendrl.auxiliary.models import ListMenuModel
 # from usmqe.web.utils import StatusIcon
 
 
-LOCATION = '/#/cluster'
+LOCATION = '/#/clusters'
 
 
 class ClustersMenuModel(ListMenuModel):
@@ -22,12 +21,6 @@ class ClustersMenuModel(ListMenuModel):
     header = PageElement(
         by=By.XPATH,
         locator="//h1[contains(text(),'Clusters')]")
-    import_btn = form.Button(
-        By.XPATH,
-        '//button[@ng-click="clusterCntrl.importCluster()"]')
-    create_btn = form.Button(
-        By.XPATH,
-        '//button[@ng-click="clusterCntrl.createCluster()"]')
 
 
 class ClustersListModel(contentviews.ListViewModel):
@@ -50,60 +43,25 @@ class ClustersRowModel(contentviews.ListViewRowModel):
         locator='.//div[contains(@class, "cluster-name")]')
     name = name_text
 
+    cluster_version = PageElement(
+        by=By.XPATH,
+        locator='.//div[contains(text(),"Cluster Version")]'
+        '/following-sibling::*')
+
+    managed = PageElement(
+        by=By.XPATH,
+        locator='.//div[contains(text(),"Managed")]/following-sibling::*')
+
+    volume_profile = PageElement(
+        by=By.XPATH,
+        locator='.//div[contains(text(),"Volume Profile")]'
+        '/following-sibling::*')
+
+    import_btn = form.Button(
+        By.XPATH,
+        '//button[@ng-click="clusterCntrl.goToImportFlow(cluster)"]')
 # TODO
-# Uncomment when fields are present on the page
-#    usage_percent_text = PageElement(
-#        by=By.XPATH,
-#        locator='.//div[contains(@class, \'percentage-heading\')]')
-#    usage_text = PageElement(
-#        by=By.XPATH,
-#        locator='.//div[contains(@class, \'percentage-used\')]')
-#    disk_info_text = PageElement(
-#        by=By.XPATH,
-#        locator='.//div[contains(@class, \'disk-info-cart\')]')
-#
-#    iops_value = PageElement(
-#        by=By.XPATH,
-#        locator='.//chart-column[@column-id=\'IOPS-0\']')
-#    iops_text = PageElement(
-#        by=By.XPATH,
-#        locator='.//chart-column[@column-id=\'IOPS-0\']/chart-gauge')
-
-    hosts_label = PageElement(
-        by=By.XPATH,
-        locator='.//div[contains(text(),"Hosts")]')
-    hosts_value = PageElement(
-        by=By.XPATH,
-        locator='.//div[contains(text(),"Hosts")]/following-sibling::*')
-
-    file_share_label = PageElement(
-        by=By.XPATH,
-        locator='.//div[contains(text(),"File Shares")]')
-    file_share_value = PageElement(
-        by=By.XPATH,
-        locator='.//div[contains(text(),"File Shares")]/following-sibling::*')
-
-#    pools_value = PageElement(
-#        by=By.XPATH,
-#        locator='.//div[@ng-bind=\'cluster.no_of_volumes_or_pools\']')
-#    pools_text = PageElement(
-#        by=By.XPATH,
-#        locator='.//div[@ng-bind=\'cluster.no_of_volumes_or_pools\']'
-#                '/preceding-sibling::div')
-
-    alerts_label = PageElement(
-        by=By.XPATH,
-        locator='.//div[contains(text(),"Alerts")]')
-    alerts_value = PageElement(
-        by=By.XPATH,
-        locator='.//div[contains(text(),"Alerts")]/following-sibling::*')
-
-    menu_link = PageElement(By.ID, "dropdownKebabRight12")
+# add link to grafana when available
 
 
-class ClustersRowMenuModel(dropdown.DropDownMenuModel):
-    """ menu availalble for a cluster/row """
-    expand_link = PageElement(by=By.LINK_TEXT, locator='Expand')
-    shrink_link = PageElement(by=By.LINK_TEXT, locator='Shrink')
-    enable_link = PageElement(by=By.LINK_TEXT, locator='Manage')
-    disable_link = PageElement(by=By.LINK_TEXT, locator='UnManage')
+# TODO add HostsListModel and HostsRowModel
--- usmqe/web/tendrl/mainpage/clusters/cluster_list/pages.py
+++ usmqe/web/tendrl/mainpage/clusters/cluster_list/pages.py
@@ -6,7 +6,6 @@ Clusters page abstraction.
 import copy
 
 from webstr.patternfly.contentviews import pages as contentviews
-import webstr.patternfly.dropdown.pages as dropdown
 
 import usmqe.web.tendrl.mainpage.clusters.cluster_list.models as m_cluster_list
 from usmqe.web.tendrl.auxiliary.pages import ListMenu
@@ -20,7 +19,7 @@ class ClustersMenu(ListMenu, ClustersWorkBase):
     _model = m_cluster_list.ClustersMenuModel
     _label = 'cluster page top menu'
     _required_elems = copy.deepcopy(ListMenu._required_elems)
-    _required_elems.extend(['header', 'import_btn'])
+    _required_elems.extend(['header'])
 
 
 class ClustersRow(contentviews.ListViewRow):
@@ -28,93 +27,34 @@ class ClustersRow(contentviews.ListViewRow):
     Cluster in Clusters list
     """
     _model = m_cluster_list.ClustersRowModel
-    _required_elems = [
-        'name_text',
-        'hosts_label',
-        'hosts_value',
-        'alerts_label',
-        'alerts_value',
-        'menu_link']
+    _required_elems = ['name_text']
 
     @property
     def name(self):
         """ returns cluster name """
         return self._model.name_text.text
 
-# TODO
-# Coming soon...
-# waiting for the model, see models.py
-#    @property
-#    def status(self):
-#        """ returns status on behalf of status_icon """
-#        return self._model.status_icon.get_attribute('title')
-
-    @property
-    def hosts_nr(self):
-        """
-        returns number of hosts in the cluster as string
-            text value of hosts_value field
-        """
-        return self._model.hosts_value.text
-
-    @property
-    def file_shares_nr(self):
-        """
-        returns number of file shares in the cluster as string
-            text value of file_shares_value field
-        """
-        return self._model.file_shares_value.text
-
-    @property
-    def pools_nr(self):
-        """
-        returns number of pools in the cluster as string
-            text value of pools_value field
-        """
-        return self._model.pools_value.text
-
-    @property
-    def alerts_nr(self):
-        """
-        returns number of alerts related to the cluster as string
-            text value of alerts_value field
-        """
-        return self._model.alerts_value.text
-
-    def click_on_alerts(self):
-        """
-        click on alerts number
-        """
-        self._model.alerts_value.click()
-
-    def open_menu(self):
-        """
-        open a dropdown menu for the row
-
-        Returns:
-            ClustersRowMenu instance
-        """
-        self._model.menu_link.click()
-        return ClustersRowMenu(self.driver)
-
     def open_details(self):
         """
-        open cluster details
-        click on cluster name
+        click on chosen cluster
         """
-        self._model.name.click()
+        self._model._root.click()
 
+    def click_on_import(self):
+        """
+        click on Import Cluster button
+        """
+        self._model.import_btn.click()
 
-# Coming soon...
-class ClustersRowMenu(dropdown.DropDownMenu):
-    """ menu availalble for a cluster/row """
-    _model = m_cluster_list.ClustersRowMenuModel
-    _required_elems = [
-      'expand_link',
-      'shrink_link'
-    ]
-
-# TODO use menu
+    @property
+    def managed(self):
+        """
+        returns if the cluster is managed
+        """
+        if self._model.managed.text.lower() == 'no':
+            return False
+        else:
+            return True
 
 
 class ClustersList(contentviews.ListView):
