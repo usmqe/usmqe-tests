f056db2f7d1ba810a2e4012bc08c8f1ee6b31a2b
Elena Bondarenko <ebondare@redhat.com>
2018-10-02 16:03:29 +0000

    Use compare_structure function

--- usmqe_tests/api/grafana/test_brick_dashboard.py
+++ usmqe_tests/api/grafana/test_brick_dashboard.py
@@ -38,32 +38,20 @@ def test_brick_dashboard_layout():
     Check that layout of dashboard is according to specification:
     ``https://github.com/Tendrl/specifications/issues/230``
     """
-    api = grafanaapi.GrafanaApi()
+    grafana = grafanaapi.GrafanaApi()
     """@pylatest grafana/layout
     .. test_step:: 1
 
-        Send **GET** request to:
-        ``GRAFANA/dashboards/db/brick-dashboard``.
+       Send **GET** request to:
+       ``GRAFANA/dashboards/db/cluster-dashboard`` and get layout structure.
+       Compare structure of panels and rows as defined in specification:
+       ``https://github.com/Tendrl/specifications/issues/222``
 
     .. test_result:: 1
 
-        JSON structure containing data related to layout is returned.
+       Defined structure and structure from Grafana API are equivalent.
     """
-    layout = api.get_dashboard("brick-dashboard")
-    pytest.check(
-        len(layout) > 0,
-        "cluster-dashboard layout should not be empty")
 
-    """@pylatest grafana/layout
-    .. test_step:: 2
-
-        Compare structure of panels and rows as defined in specification:
-        ``https://github.com/Tendrl/specifications/issues/230``
-
-    .. test_result:: 2
-
-        Defined structure and structure from Grafana API are equivalent.
-    """
     structure_defined = {
         'At-a-Glance': [
             'Status',
@@ -73,25 +61,12 @@ def test_brick_dashboard_layout():
             'Weekly Growth Rate',
             'Weeks Remaining',
             'Healing',
-            'IOPS',
+            'Brick IOPS',
             'LVM Thin Pool Meta Data %',
             'LVM Thin Pool Data Usage %'],
         'Disk Load': [
             'Throughput',
-            'IOPS',
-            'Latency',
-            'Mount Point Utilization']}
-    structure = {}
-    for row in layout["dashboard"]["rows"]:
-        structure[row["title"]] = []
-        for panel in row["panels"]:
-            if panel["title"]:
-                structure[row["title"]].append(panel["title"])
-            elif "displayName" in panel.keys() and panel["displayName"]:
-                structure[row["title"]].append(panel["displayName"])
+            'Disk IOPS',
+            'Latency']}
 
-    LOGGER.debug("defined layout structure = {}".format(structure_defined))
-    LOGGER.debug("layout structure in grafana = {}".format(structure))
-    pytest.check(
-        structure_defined == structure,
-        "defined structure of panels should equal to structure in grafana")
+    grafana.compare_structure(structure_defined, "brick-dashboard")
