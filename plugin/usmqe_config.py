7e85df9178ca13c2c9fc83d19fe6e2656bab6ec7
Filip Balák <fbalak@redhat.com>
2018-10-08 10:40:30 +0200

    turn config into object

--- plugin/usmqe_config.py
+++ plugin/usmqe_config.py
@@ -3,69 +3,86 @@
 Pytest plugin to handle usmqe ini config files.
 
 .. moduleauthor:: Martin Kudlej <mkudlej@redhat.com>
+.. moduleauthor:: Filip Balak <fbalak@redhat.com>
 """
 
 
 from configparser import ConfigParser
 
 import pytest
-
+import os
+import argparse
+import yaml
 import usmqe.inventory
 
 
-def pytest_addoption(parser):
+class UsmConfig(object):
     """
-    Add ini options to be accepted by pytest.
+    Configuration object containing inventory hosts file and configuration
+    specified in usm yaml configuration file.
     """
-    # defaults are specified in root pytest.ini file
-    parser.addini('usm_config', 'USM configuration')
-    parser.addini('usm_inventory', 'USM host configuration')
-    parser.addini(
-        'skip_invalid',
-        'Tells if not valid tests should be skipped, default True',
-        'bool', default=True)
+
+    def __init__(self):
+        self.hosts = {}
+        self.usm = {}
+        self.pytest = {}
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--config-file",
+            nargs='?',
+            help="path to file where is usm yaml configuration")
+        parser.add_argument(
+            "--inventory-file",
+            nargs='?',
+            help="path to usm inventory file")
+        args = parser.parse_args()
+
+        # get default usm.yaml from conf/usm.yaml
+        if not args.config_file:
+            config_file = os.path.join(os.getcwd(), "conf", "usm.yaml")
+        else:
+            try:
+                config_file = args.config_file
+            except FileNotFoundError() as err:
+                print("Configuration file {} does not exist.".format(
+                    args.config_file))
+        self.usm = self.load_config(config_file)
+
+        # get default inventory file from conf/usm.hosts
+        if not args.inventory_file:
+            inventory_file = os.path.join(os.getcwd(), "conf", "usm.hosts")
+        else:
+            try:
+                inventory_file = args.inventory_file
+            except FileNotFoundError() as err:
+                print("Inventory file {} does not exist.".format(
+                    args.inventory_file))
+        # self.hosts = self.load_inventory(inventory_file)
 
 
-@pytest.fixture(autouse=True, scope="session")
-def load_inventory():
-    """
-    Load inventory file to module inventory.py.
+    def load_inventory(self, inventory_file):
+        """
+        Load inventory file to module inventory.py.
 
-    To use content from inventory file just *import inventory* and then use
-    proper function from ``usmqe.inventory``.
-    Name of inventory file is stored in ``usm_inventory`` option in
-    ``pytest.ini``.  Its value can be overriden by ``pytest -o
-    usm_inventory=path``.
-    """
-    # update machine config (reading ansible inventory)
-    hosts = ConfigParser(allow_no_value=True)
-    hosts.read(pytest.config.getini("usm_inventory"))
-    for rolename in hosts.sections():
-        for hostname, _ in hosts.items(rolename):
-            usmqe.inventory.add_host_entry(rolename, hostname)
+        To use content from inventory file just *import inventory* and then use
+        proper function from ``usmqe.inventory``.
+        Name of inventory file is stored in ``usm_inventory`` option in
+        ``pytest.ini``.  Its value can be overriden by ``pytest -o
+        usm_inventory=path``.
+        """
+        # update machine config (reading ansible inventory)
+        hosts = ConfigParser(allow_no_value=True)
+        hosts.read(inventory_file)
+        for rolename in hosts.sections():
+            for hostname, _ in hosts.items(rolename):
+                usmqe.inventory.add_host_entry(rolename, hostname)
 
 
-@pytest.fixture(autouse=True, scope="session")
-def load_config():
-    """
-    Loads configuration from pytest.ini file.
-
-    If there is configured external configuration file(usm_config)
-    then ini values are updated by values from configuration file.
-
-    All configuration entries can be overriden by::
-
-        $ py.test -o=usm_username=admin2
-    """
-    if pytest.config.getini("usm_config"):
-        conf = ConfigParser()
-        conf.read(pytest.config.getini("usm_config"))
-        if "usmqepytest" not in conf.sections():
-            # TODO: report a problem
-            return
-        for key, value in conf.items("usmqepytest"):
-            override_value = pytest.config._get_override_ini_value(key)
-            if override_value is None:
-                pytest.config._inicache[key] = value
-            else:
-                pytest.config._inicache[key] = override_value
+    def load_config(self, config_file):
+        """
+        Loads configuration from pytest.yaml file.
+        """
+        with open(config_file) as config_open:
+            conf = yaml.load(config_open)
+        return conf
