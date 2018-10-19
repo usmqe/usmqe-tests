75b69759f1e1eacda5dfbd255bb5c9b620444502
Elena Bondarenko <ebondare@redhat.com>
2018-10-19 14:31:24 +0200

    Add entity user and users collection

---
+++ usmqe/base/application/entities/users.py
@@ -0,0 +1,23 @@
+import attr
+from navmazing import NavigateToAttribute
+
+from usmqe.base.application.entities import BaseCollection, BaseEntity
+from usmqe.base.application.views.user import UsersView
+from usmqe.base.application.implementations.web_ui import TendrlNavigateStep, ViaWebUI
+
+@attr.s
+class User(BaseEntity):
+    pass
+
+
+@attr.s
+class UsersCollection(BaseCollection):
+    ENTITY = User
+
+
+@ViaWebUI.register_destination_for(UsersCollection, "All")
+class UsersAll(TendrlNavigateStep):
+    VIEW = UsersView
+
+
+
