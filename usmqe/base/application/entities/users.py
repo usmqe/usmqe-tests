d82af14be7be7b3adbb50cb3053abecb3f4b936a
Elena Bondarenko <ebondare@redhat.com>
2018-10-24 09:38:45 +0200

    Add adduser function

--- usmqe/base/application/entities/users.py
+++ usmqe/base/application/entities/users.py
@@ -5,6 +5,15 @@ from usmqe.base.application.entities import BaseCollection, BaseEntity
 from usmqe.base.application.views.user import UsersView
 from usmqe.base.application.implementations.web_ui import TendrlNavigateStep, ViaWebUI
 
+def role_to_list(role):
+    if role == "admin":
+        return [True, False, False]
+    if role == "normal":
+        return [False, True, False]
+    if role == "limited":
+        return [False, False, True]
+    raise ValueError("Role must be either 'admin', 'normal' or 'limited'.")
+
 
 @attr.s
 class User(BaseEntity):
@@ -14,6 +23,23 @@ class User(BaseEntity):
 @attr.s
 class UsersCollection(BaseCollection):
     ENTITY = User
+
+    def adduser(self, user_id, name, email, notifications_on, password, role):
+        role_list = role_to_list(role)
+        view = ViaWebUI.navigate_to(self, "All")
+        view.adduser.click()
+        view = application.web_ui.create_view(UsersAddView)
+        changed = view.fill({"user_id": user_id, 
+                             "users_name": name,
+                             "email": email,
+                             "notifications_on": notifications_on,
+                             "password": password,
+                             "confirm_password": password,
+                             "is_admin": role_list[0],
+                             "is_normal_user": role_list[1],
+                             "is_limited_user": role_list[2]})
+        if changed:
+            view.save_button.click()
 
 
 @ViaWebUI.register_destination_for(UsersCollection, "All")
