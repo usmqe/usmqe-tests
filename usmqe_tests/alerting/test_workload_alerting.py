1a4fa959355f91abc4f7e7ec174797f1a4c72e2b
Elena Bondarenko <ebondare@redhat.com>
2018-10-25 14:48:55 +0000

    Change the docstring style

--- usmqe_tests/alerting/test_workload_alerting.py
+++ usmqe_tests/alerting/test_workload_alerting.py
@@ -10,34 +10,41 @@ import usmqe.usmmail
 LOGGER = pytest.get_logger('workload_alerting', module=True)
 
 
+@pytest.mark.author("ebondare@redhat.com")
 def test_cpu_alerts(workload_cpu_utilization):
     """
-    Alerting: cpu_utilization
-    *******************
-
-    .. test_metadata:: author ebondare@redhat.com
-
-    Description
-    ===========
-
     Check that Tendrl sends no CPU Utilization alerts if utilization is below 70,
     it sends CPU Utilization warnings if CPU Utilization is between 70 and 90
     and it sends CPU Utilization critical alerts if utilization is above 90.
-
     """
-    # Get the fixture data and widen the interval for the messages to arrive
+    """
+    :step:
+      Get the messages that arrived in the interval provided by cpu utilization fixture
+      or a little later
+    :result:
+      The list of all relevant messages
+    """
+    EXTRA_TIME = 30
     from_timestamp = workload_cpu_utilization["start"].timestamp()
-    until_timestamp = workload_cpu_utilization["end"].timestamp() + 30
-    LOGGER.debug("Fixture start timestamp: {} ({})".format(from_timestamp, workload_cpu_utilization["start"]))
-    LOGGER.debug("Fixture end timestamp: {} ({})".format(until_timestamp - 30, workload_cpu_utilization["end"]))
+    until_timestamp = workload_cpu_utilization["end"].timestamp() + EXTRA_TIME
+    LOGGER.debug("Fixture start timestamp: {} ({})".format(from_timestamp,
+                                                           workload_cpu_utilization["start"]))
+    LOGGER.debug("Fixture end timestamp: {} ({})".format(until_timestamp - EXTRA_TIME,
+                                                         workload_cpu_utilization["end"]))
     LOGGER.debug("Fixture result is: {}".format(workload_cpu_utilization["result"]))
 
     # Wait until the messages surely arrive
-    time.sleep(30)
+    time.sleep(EXTRA_TIME)
     messages = usmqe.usmmail.get_msgs_by_time(start_timestamp=from_timestamp,
                                               end_timestamp=until_timestamp)
     LOGGER.debug("Selected messages count: {}".format(len(messages)))
 
+    """
+    :step:
+      Check that the type of alert corresponds to the workload
+    :result:
+      If the workload was low, there's no alert. If it's high, the correct alert was received.
+    """
     # Distinguish between warnings and critical alerts
     cpu_warning = False
     cpu_critical = False
