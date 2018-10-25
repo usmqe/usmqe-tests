a0cc416b68a7b5227ada705f0f01f90c8bc00dd3
Elena Bondarenko <ebondare@redhat.com>
2018-10-25 15:20:17 +0200

    Trying to delete a user, so far unsuccessfully

--- usmqe/base/application/entities/users.py
+++ usmqe/base/application/entities/users.py
@@ -1,4 +1,5 @@
 import attr
+import pytest
 from navmazing import NavigateToAttribute
 from wait_for import wait_for
 
@@ -7,10 +8,12 @@ from usmqe.base.application.views.user import UsersView
 from usmqe.base.application.views.adduser import AddUserView
 from usmqe.base.application.implementations.web_ui import TendrlNavigateStep, ViaWebUI
 
+LOGGER = pytest.get_logger('ui_users_testing', module=True)
+
 
 @attr.s
 class User(BaseEntity):
-    pass
+    user_id = attr.ib()
 
 
 @attr.s
@@ -31,6 +34,15 @@ class UsersCollection(BaseCollection):
                              "role": role})
         if changed:
             view.save_button.click()
+
+    def delete_user(self, user_id):
+        view = ViaWebUI.navigate_to(self, "All")
+        wait_for(lambda: view.is_displayed, timeout=5)
+        for row in view.users:
+            LOGGER.debug("Current User ID: ".format(row[0]))
+            if row[0] == user_id:
+                row["6"].select("Delete User", close=False)
+                wait_for(lambda: view.is_displayed, timeout=5)
 
 
 @ViaWebUI.register_destination_for(UsersCollection, "All")
--- usmqe_tests/ui/test_user.py
+++ usmqe_tests/ui/test_user.py
@@ -1,5 +1,9 @@
 from usmqe.base.application import Application
 from usmqe.base.application.implementations.web_ui import ViaWebUI
+import pytest
+
+LOGGER = pytest.get_logger('ui_user_testing', module=True)
+
 
 def test_add_del_normal_user():
     app = Application(hostname="ebondare-usm1-server.usmqe.lab.eng.brq.redhat.com", scheme="http", username="admin", password="adminuser")
@@ -10,8 +14,13 @@ def test_add_del_normal_user():
                                                 password="1234567890",
                                                 role="normal")
     limited_user = app.collections.users.adduser(user_id="limited_user_auto",
-                                                name="Autotest Read-Only User",
+                                                name="Read-Only User",
                                                 email="limited_user@tendrl.org",
                                                 notifications_on=True,
                                                 password="1234567890",
                                                 role="limited")
+
+def test_del_normal_user():
+    app = Application(hostname="ebondare-usm1-server.usmqe.lab.eng.brq.redhat.com", scheme="http", username="admin", password="adminuser")
+    app.collections.users.delete_user("normal_user_auto")
+    assert False
