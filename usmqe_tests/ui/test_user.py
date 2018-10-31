4ffb12ea5eea8117ee94a4225599f58ff7716907
Elena Bondarenko <ebondare@redhat.com>
2018-10-31 13:50:34 +0100

    Add parameters

--- usmqe_tests/ui/test_user.py
+++ usmqe_tests/ui/test_user.py
@@ -1,14 +1,14 @@
 import pytest
 
-LOGGER = pytest.get_logger('ui_user_testing', module=True)
+#LOGGER = pytest.get_logger('ui_user_testing', module=True)
 
 
-@pytest.mark.parametrize("role", ["normal", "limited", "admin"])
+@pytest.mark.parametrize("role", ["normal", "limited"])
 def test_user_crud(application, role):
     user = application.collections.users.create(
         user_id="{}_user_auto".format(role),
-        name="Autotest Normal User",
-        email="user@tendrl.org",
+        name="Autotest {} User".format(role),
+        email="{}user@tendrl.org".format(role),
         notifications_on=True,
         password="1234567890",
         role=role
@@ -16,3 +16,4 @@ def test_user_crud(application, role):
     assert user.exists
     user.delete()
     assert not user.exists
+
