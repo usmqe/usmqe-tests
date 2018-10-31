cf96c5770515bd30e6c9bc0fd2df0173373c4236
Filip Balák <fbalak@redhat.com>
2018-10-31 17:09:14 +0100

    add total_host_memory fixture

--- usmqe_tests/api/grafana/conftest.py
+++ usmqe_tests/api/grafana/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import usmqe.usmssh as usmssh
 from usmqe.api.tendrlapi import glusterapi
 
 LOGGER = pytest.get_logger('grafana_conftest', module=True)
@@ -14,3 +15,15 @@ def up_gluster_nodes(valid_session_credentials):
     cluster_list = api.get_nodes()
     return [x for x in cluster_list["nodes"]
             if "tendrl/node" in x["tags"] and x["status"] == "UP"]
+
+@pytest.fixture
+def total_host_memory():
+    """
+    Get total memory of host in bytes.
+    """
+    SSH = usmssh.get_ssh()
+    host = pytest.config.getini("usm_cluster_member")
+    meminfo_cmd = "free -b | awk '{if (NR==2) print $2}'"
+    _, stdout, _ = SSH[host].run(meminfo_cmd)
+    mem_total = stdout.decode("utf-8")
+    return mem_total
