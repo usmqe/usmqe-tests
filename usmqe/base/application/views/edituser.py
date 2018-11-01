b1031b0babec271e4247a8bc159fad132174668d
Elena Bondarenko <ebondare@redhat.com>
2018-11-01 12:16:45 +0100

    Add Edit User view

---
+++ usmqe/base/application/views/edituser.py
@@ -0,0 +1,25 @@
+from widgetastic.widget import (GenericLocatorWidget, ParametrizedLocator, ParametrizedView, Text,
+                                TextInput, Table, BaseInput)
+
+from usmqe.base.application.views.common import BaseLoggedInView
+from usmqe.base.application.widgets import BootstrapSwitch, Kebab, RadioGroup
+
+from taretto.ui.patternfly import Button, Dropdown, BreadCrumb
+
+
+class EditUserView(BaseLoggedInView):
+    page_breadcrumb = BreadCrumb()
+    user_id = Text(".//p[class='ng-binding']")
+    users_name = TextInput(name="name")
+    email = TextInput(name="userEmail")
+    # notifications_on = BootstrapSwitch()
+    # TODO: fix the switch
+    password = TextInput(name="password")
+    confirm_password = TextInput(name="confirmPassword")
+    role = RadioGroup(".//div[./label[@for='role']]")
+    save_button = Button("Save")
+    cancel_button = Button("Cancel")
+
+    @property
+    def is_displayed(self):
+        return False
