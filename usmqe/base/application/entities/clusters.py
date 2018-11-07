164d6a998735e8964a743f6b1bdd10014838128d
Elena Bondarenko <ebondare@redhat.com>
2018-11-07 11:15:51 +0100

    Fix a couple of mistakes in cluster entity

--- usmqe/base/application/entities/clusters.py
+++ usmqe/base/application/entities/clusters.py
@@ -3,10 +3,9 @@ from navmazing import NavigateToAttribute, NavigateToSibling
 from wait_for import wait_for
 
 from usmqe.base.application.entities import BaseCollection, BaseEntity
-from usmqe.base.application.views.user import UsersView
+from usmqe.base.application.views.cluster import ClustersView
 from usmqe.base.application.views.common import DeleteConfirmationView
-from usmqe.base.application.views.adduser import AddUserView
-from usmqe.base.application.views.edituser import EditUserView
+from usmqe.base.application.views.importcluster import ImportClusterView
 from usmqe.base.application.implementations.web_ui import TendrlNavigateStep, ViaWebUI
 
 
@@ -15,17 +14,17 @@ class Cluster(BaseEntity):
     name = attr.ib()
     version = attr.ib()
     managed = attr.ib()
-    hosts = attr.ib()
+    # hosts_number = attr.ib()
     status = attr.ib()
     # attributes below are not defined until cluster is imported
-    volumes = attr.ib()
-    alerts = attr.ib()
-    profiling = attr.ib()
+    # volumes = attr.ib()
+    # alerts = attr.ib()
+    # profiling = attr.ib()
 
 
-    def import(self, cancel=False):
-        pass
-
+    # def import(self, cancel=False):
+    #    pass
+    # import will belong to an individual cluster when the widget is ready
 
     def unmanage(self, cancel=False):
         pass
@@ -52,11 +51,26 @@ class Cluster(BaseEntity):
 class ClustersCollection(BaseCollection):
     ENTITY = Cluster
 
+    def cluster_import(self, cluster_name):
+        view = ViaWebUI.navigate_to(self, "Import")
+        view.fill({"cluster_name": cluster_name})
+        view.save_button.click()
+        return self.instantiate(cluster_name, "RHGS 3.4", "Yes", "Ready to Use")
 
-@ViaWebUI.register_destination_for(UsersCollection, "All")
+
+@ViaWebUI.register_destination_for(ClustersCollection, "All")
 class UsersAll(TendrlNavigateStep):
     VIEW = ClustersView
     prerequisite = NavigateToAttribute("application.web_ui", "LoggedIn")
 
     def step(self):
         self.parent.navbar.clusters.select_item("All Clusters")
+
+
+@ViaWebUI.register_destination_for(ClustersCollection, "Import")
+class UsersAll(TendrlNavigateStep):
+    VIEW = ImportClusterView
+    prerequisite = NavigateToSibling("All")
+
+    def step(self):
+        self.parent.import_button.click()
