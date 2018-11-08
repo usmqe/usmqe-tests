93fe7e45cc99501a3a978f06771f7ef96559f42a
Elena Bondarenko <ebondare@redhat.com>
2018-11-08 16:09:39 +0100

    Add more user tests

--- usmqe_tests/ui/test_user.py
+++ usmqe_tests/ui/test_user.py
@@ -2,19 +2,36 @@ import pytest
 from usmqe.base.application.implementations.web_ui import ViaWebUI
 #LOGGER = pytest.get_logger('ui_user_testing', module=True)
 import time
+import copy
+import json
+
+from usmqe.api.tendrlapi import user as tendrlapi_user
+from usmqe.api.tendrlapi.common import login, logout
+
 
 
 @pytest.mark.parametrize("role", ["normal", "limited"])
-def test_user_crud(application, role):
+def test_user_crud(application, role, valid_session_credentials):
     user = application.collections.users.create(
         user_id="{}_user_auto".format(role),
         name="{} user".format(role),
         email="{}user@tendrl.org".format(role),
-        notifications_on=True,
+        notifications_on=False,
         password="1234567890",
         role=role
     )
-    assert user.exists
+    # assert user.exists
+    test = tendrlapi_user.ApiUser(auth=valid_session_credentials)
+    user_data = {
+        "name": user.user_id,
+        "username": user.name,
+        "email": user.email,
+        "role": user.role,
+        "password": user.password,
+        "email_notifications": user.notifications_on}
+
+    # test.check_user(user_data)
+
     user.edit({
               "user_id": user.user_id,
               "users_name": user.name,
@@ -27,3 +44,26 @@ def test_user_crud(application, role):
     assert user.exists
     user.delete()
     assert not user.exists
+
+
+def test_user_creation_password_invalid(application, valid_session_credentials, 
+                                        valid_normal_user_data, invalid_password):
+    user = application.collections.users.create(
+        user_id=valid_normal_user_data["username"],
+        name=valid_normal_user_data["name"],
+        email=valid_normal_user_data["email"],
+        notifications_on=valid_normal_user_data["email_notifications"],
+        password=invalid_password,
+        role=valid_normal_user_data["role"]
+    )
+    assert not user.exists
+    test = tendrlapi_user.ApiUser(auth=valid_session_credentials)
+
+    user_data_password_invalid = copy.deepcopy(valid_normal_user_data)
+    user_data_password_invalid["username"] = invalid_password
+    asserts = {
+        "ok": False,
+        "reason": 'Not Found',
+        "status": 404}
+    not_found = test.get_user(user_data_password_invalid["username"], asserts_in=asserts)
+    assert "Not found" in str(not_found)
