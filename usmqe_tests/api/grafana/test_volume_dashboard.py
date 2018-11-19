50fb1bc8719c5a2fcb4c18cf1cd62b7b2c4e284b
Filip Balák <fbalak@redhat.com>
2018-11-19 13:33:40 +0100

    add test_capacity_utilization_gauge test

--- usmqe_tests/api/grafana/test_volume_dashboard.py
+++ usmqe_tests/api/grafana/test_volume_dashboard.py
@@ -3,7 +3,9 @@ REST API test suite - Grafana dashboard volume-dashboard
 """
 
 import pytest
+import time
 from usmqe.api.grafanaapi import grafanaapi
+from usmqe.api.graphiteapi import graphiteapi
 
 
 LOGGER = pytest.get_logger('volume_dashboard', module=True)
@@ -53,3 +55,58 @@ def test_volume_dashboard_layout():
             'File Operations for Inode Operations',
             'File Operations for Entry Operations']}
     grafana.compare_structure(structure_defined, "volume-dashboard")
+
+
+@pytest.mark.author("fbalak@redhat.com")
+def test_capacity_utilization_gauge(
+        workload_capacity_utilization, cluster_reuse):
+    """
+    Check that Grafana panel *Capacity Utilization* gauge chart is showing
+    correct values.
+    """
+    # TODO(fbalak): get this number dynamically
+    # number of samples from graphite target per minute
+    if cluster_reuse["short_name"]:
+        cluster_identifier = cluster_reuse["short_name"]
+    else:
+        cluster_identifier = cluster_reuse["integration_id"]
+
+    grafana = grafanaapi.GrafanaApi()
+    graphite = graphiteapi.GraphiteApi()
+
+    capacity_panel = grafana.get_panel(
+        "Capacity Utilization",
+        row_title="Capacity",
+        dashboard="volume-dashboard")
+    print(capacity_panel)
+    exit()
+
+    """
+    :step:
+      Send **GET** request to ``GRAPHITE/render?target=[target]&format=json``
+      where [target] is part of uri obtained from previous GRAFANA call.
+      There should be target for capacity utilization of a host.
+      Compare number of hosts from Graphite with value retrieved from
+      ``workload_capacity_utilization`` fixture.
+    :result:
+      JSON structure containing data related to capacity utilization is similar
+      to values set by ``workload_capacity_utilization`` fixture in given time.
+    """
+    # get graphite target pointing at data containing number of host
+    targets = grafana.get_panel_chart_targets(capacity_panel, cluster_identifier)
+    targets_used = (targets[0][0], targets[1][0], targets[-1][0])
+    for key, target_expected in enumerate((
+            "capacity.percent-buffered",
+            "capacity.percent-cached",
+            "capacity.percent-used")):
+        pytest.check(
+            targets_used[key].endswith(target_expected),
+            "There is used target that ends with `{}`".format(target_expected))
+    # make sure that all data in graphite are saved
+    time.sleep(2)
+    graphite.compare_data_mean(
+        workload_capacity_utilization["result"],
+        targets_used,
+        workload_capacity_utilization["start"],
+        workload_capacity_utilization["end"],
+        divergence=15)
