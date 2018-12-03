454bef1dc4aa3324b7178a65f58bcc01916ae972
Elena Bondarenko <ebondare@redhat.com>
2018-12-03 14:32:39 +0100

    Add docstrings and fix a mistake in test_edit_email_password

--- usmqe_tests/ui/test_user.py
+++ usmqe_tests/ui/test_user.py
@@ -10,6 +10,8 @@ from usmqe.api.tendrlapi.common import login, logout
 from usmqe.base.application import Application
 
 
+@pytest.mark.author("ebondare@redhat.com")
+@pytest.mark.happypath
 @pytest.mark.parametrize("role", ["normal", "limited"])
 def test_user_crud(application, role, valid_session_credentials):
     user = application.collections.users.create(
@@ -46,6 +48,8 @@ def test_user_crud(application, role, valid_session_credentials):
     assert not user.exists
 
 
+@pytest.mark.author("ebondare@redhat.com")
+@pytest.mark.negative
 def test_user_creation_password_invalid(application, valid_session_credentials, 
                                         valid_normal_user_data, invalid_password):
     user = application.collections.users.create(
@@ -69,30 +73,55 @@ def test_user_creation_password_invalid(application, valid_session_credentials,
     assert "Not found" in str(not_found)
 
 
-def test_edit_email_password(valid_new_normal_user, valid_normal_user_data):
-    #new_data = {"email": "new_user_email@ya.ru"}
-    #application_normal_user.collections.users.edit_logged_in_user(valid_normal_user_data[username], new_data)
+@pytest.mark.author("ebondare@redhat.com")
+@pytest.mark.happypath
+def test_edit_email_password(valid_new_normal_user, valid_normal_user_data, valid_password):
+    """ 
+    Change user's password and email in My Settings and login with new password.
+    """
+    """ 
+    :step:
+      Log in and edit user's email and password in My Settings
+    :result:
+      User's email and password are changed
+    """
     app1 = Application(hostname="ebondare-usm1-server.usmqe.lab.eng.brq.redhat.com",
                            scheme="http",
                            username=valid_normal_user_data["username"],
                            password=valid_normal_user_data["password"])
     view = ViaWebUI.navigate_to(app1.web_ui, "LoggedIn")
     new_data = {"email": "new_user_email@ya.ru", 
-                "password": valid_normal_user_data["password"],
-                "password_confirmation": valid_normal_user_data["password"] } 
+                "password": valid_password,
+                "confirm_password": valid_password } 
     app1.collections.users.edit_logged_in_user(valid_normal_user_data["username"], new_data)
+    """
+    :step:
+      Log in using the new passord
+    :result:
+      User is able to log in with the new password
+    """
     app1.web_ui.browser_manager.quit()
     app2 = Application(hostname="ebondare-usm1-server.usmqe.lab.eng.brq.redhat.com",
                            scheme="http",
                            username=valid_normal_user_data["username"],
-                           password=valid_normal_user_data["password"])
+                           password=valid_password)
     view = ViaWebUI.navigate_to(app2.web_ui, "LoggedIn")
     app2.web_ui.browser_manager.quit()
 
 
+@pytest.mark.author("ebondare@redhat.com")
 def test_edit_email_only(valid_new_normal_user, valid_normal_user_data):
-    #new_data = {"email": "new_user_email@ya.ru"}
-    #application_normal_user.collections.users.edit_logged_in_user(valid_normal_user_data[username], new_data)
+    """ 
+    Change user's email in My Settings and login again.
+    Fails due to https://bugzilla.redhat.com/show_bug.cgi?id=1654623
+    """
+    """ 
+    :step:
+      Log in and edit user's email in My Settings
+    :result:
+      User's email is changed
+    """
+
     app1 = Application(hostname="ebondare-usm1-server.usmqe.lab.eng.brq.redhat.com",
                            scheme="http",
                            username=valid_normal_user_data["username"],
@@ -101,6 +130,14 @@ def test_edit_email_only(valid_new_normal_user, valid_normal_user_data):
     new_data = {"email": "new_user_email@ya.ru"}
     app1.collections.users.edit_logged_in_user(valid_normal_user_data["username"], new_data)
     app1.web_ui.browser_manager.quit()
+    """
+    :step:
+      Log in using the old passord
+    :result:
+      User is able to log in with the old password. 
+      Fails due to https://bugzilla.redhat.com/show_bug.cgi?id=1654623
+    """
+
     app2 = Application(hostname="ebondare-usm1-server.usmqe.lab.eng.brq.redhat.com",
                            scheme="http",
                            username=valid_normal_user_data["username"],
