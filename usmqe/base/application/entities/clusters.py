3b2d5dad87560389cc2a655360496db659bfd40d
Elena Bondarenko <ebondare@redhat.com>
2019-01-08 14:12:03 +0100

    Add some cluster management functions

--- usmqe/base/application/entities/clusters.py
+++ usmqe/base/application/entities/clusters.py
@@ -1,12 +1,17 @@
 import attr
 import time
 from navmazing import NavigateToAttribute
-# from wait_for import wait_for
+from wait_for import wait_for
+import pytest
+
 
 from usmqe.base.application.entities import BaseCollection, BaseEntity
-from usmqe.base.application.views.cluster import ClustersView
-from usmqe.base.application.views.importcluster import ImportClusterView
+from usmqe.base.application.views.cluster import ClustersView, UnmanageConfirmationView
+from usmqe.base.application.views.importcluster import ImportClusterView, ImportTaskSubmittedView
 from usmqe.base.application.implementations.web_ui import TendrlNavigateStep, ViaWebUI
+
+
+LOGGER = pytest.get_logger('clusters', module=True)
 
 
 @attr.s
@@ -14,12 +19,27 @@ class Cluster(BaseEntity):
     name = attr.ib()
     version = attr.ib()
     managed = attr.ib()
-    hosts_number = attr.ib()
+    hosts = attr.ib()
     status = attr.ib()
     # attributes below are not defined until cluster is imported
-    # volumes = attr.ib()
-    # alerts = attr.ib()
-    # profiling = attr.ib()
+    volumes = attr.ib()
+    alerts = attr.ib()
+    profiling = attr.ib()
+
+    def update(self):
+        view = self.application.web_ui.create_view(ClustersView)
+        self.version = view.clusters(self.name).cluster_version.text
+        self.managed = view.clusters(self.name).managed.text
+        self.hosts = view.clusters(self.name).hosts.text
+        self.status = view.clusters(self.name).status.text
+        if self.managed == "Yes":
+            self.volumes = view.clusters(self.name).volumes.text
+            self.alerts = view.clusters(self.name).alerts.text
+            self.profiling = view.clusters(self.name).profiling.text
+        else:
+            self.volumes = None
+            self.alerts = None
+            self.profiling = None
 
     def cluster_import(self, cluster_name=None):
         view = ViaWebUI.navigate_to(self, "Import")
@@ -27,16 +47,41 @@ class Cluster(BaseEntity):
             view.fill({"cluster_name": cluster_name})
             self.name = cluster_name
         view.confirm_import.click()
-        # TODO: update cluster attributes
+        time.sleep(1)
+        view = self.application.web_ui.create_view(ImportTaskSubmittedView)
+        view.close_button.click()
+        time.sleep(180)
+        self.update()
+        LOGGER.debug("Cluster is managed: {}".format(self.managed))
+        pytest.check(self.managed == "Yes")
 
     def unmanage(self, cancel=False):
-        pass
+        view = self.application.web_ui.create_view(ClustersView)
+        view.clusters(self.name).actions.select("Unmanage")
+        view = self.application.web_ui.create_view(UnmanageConfirmationView)
+        wait_for(lambda: view.is_displayed, timeout=3)
+        view.unmanage.click()
+        time.sleep(240)
+        self.update()
+        LOGGER.debug("Cluster is managed: {}".format(self.managed))
+        pytest.check(self.managed == "No")
 
     def enable_profiling(self, cancel=False):
-        pass
+        view = self.application.web_ui.create_view(ClustersView)
+        view.clusters(self.name).actions.select("Enable Profiling")
+        time.sleep(180)
+        self.update()
+        LOGGER.debug("Cluster profiling value: {}".format(self.profiling))
+        pytest.check(self.profiling == "Enabled")
+
 
     def disable_profiling(self, cancel=False):
-        pass
+        view = self.application.web_ui.create_view(ClustersView)
+        view.clusters(self.name).actions.select("Disable Profiling")
+        time.sleep(180)
+        self.update()
+        LOGGER.debug("Cluster profiling value: {}".format(self.profiling))
+        pytest.check(self.profiling == "Disabled")
 
     def expand(self, cancel=False):
         pass
@@ -58,13 +103,28 @@ class ClustersCollection(BaseCollection):
         view = ViaWebUI.navigate_to(self, "All")
         clusters_list = []
         for cluster_id in self.get_all_cluster_ids():
-            cluster = self.instantiate(
-                cluster_id,
-                view.clusters(cluster_id).cluster_version.text,
-                view.clusters(cluster_id).managed.text,
-                view.clusters(cluster_id).hosts.text,
-                view.clusters(cluster_id).status.text)
-            clusters_list.append(cluster)
+            if view.clusters(cluster_id).managed.text == "No":
+                cluster = self.instantiate(
+                    cluster_id,
+                    view.clusters(cluster_id).cluster_version.text,
+                    view.clusters(cluster_id).managed.text,
+                    view.clusters(cluster_id).hosts.text,
+                    view.clusters(cluster_id).status.text,
+                    None,
+                    None,
+                    None)
+                clusters_list.append(cluster)
+            else:
+                cluster = self.instantiate(
+                    cluster_id,
+                    view.clusters(cluster_id).cluster_version.text,
+                    view.clusters(cluster_id).managed.text,
+                    view.clusters(cluster_id).hosts.text,
+                    view.clusters(cluster_id).status.text,
+                    view.clusters(cluster_id).volumes.text,
+                    view.clusters(cluster_id).alerts.text,
+                    view.clusters(cluster_id).profiling.text)
+                clusters_list.append(cluster)
         return clusters_list
 
 
