d5c9450659214a1e330932bba31e0092d009e0f2
Filip Balák <fbalak@redhat.com>
2019-01-11 10:00:47 +0100

    fix getting cluster_identifier

--- usmqe_tests/alerting/conftest.py
+++ usmqe_tests/alerting/conftest.py
@@ -9,7 +9,8 @@ def default_entities(cluster_reuse):
     Returns basic items that are occuring in alert messages and can be used
     for substitution.
     """
-    cluster_identifier = cluster_reuse["cluster_name"] or cluster_reuse["cluster_id"]
+    cluster_identifier = cluster_reuse[
+        "short_name"] or cluster_reuse["cluster_id"]
     node = CONF.config["usmqe"]["cluster_member"]
     return {
             "cluster": cluster_identifier,
