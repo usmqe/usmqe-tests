b03b2507946bc41dd9a6b4e2f20ef39d912b2f0d
Elena Bondarenko <ebondare@redhat.com>
2019-01-21 11:00:44 +0100

    Add events entity, view and basic test

---
+++ usmqe/base/application/entities/events.py
@@ -0,0 +1,72 @@
+import attr
+import pytest
+import time
+
+from usmqe.base.application.entities import BaseCollection, BaseEntity
+from usmqe.base.application.implementations.web_ui import ViaWebUI
+
+from usmqe.base.application.views.event import ClusterEventsView
+from usmqe.base.application.views.task import TaskEventsView
+
+
+LOGGER = pytest.get_logger('events', module=True)
+
+
+@attr.s
+class Event(BaseEntity):
+    event_id = attr.ib()
+    description = attr.ib()
+    date = attr.ib()
+    cluster_name = attr.ib()
+
+
+@attr.s
+class EventsCollection(BaseCollection):
+    ENTITY = Event
+
+    def get_all_event_ids(self):
+        view = self.application.web_ui.create_view(ClusterEventsView)
+        time.sleep(2)
+        return view.all_event_ids
+
+    def get_events(self):
+        view = ViaWebUI.navigate_to(self.parent, "Events")
+        event_list = []
+        for event_id in self.get_all_event_ids():
+            event = self.instantiate(
+                event_id,
+                view.events(event_id).description.text,
+                view.events(event_id).date.text,
+                view.cluster_name.text)
+            event_list.append(event)
+        return event_list
+
+@attr.s
+class TaskEvent(Event):
+    event_type = attr.ib()
+    # task_id = attr.ib()
+
+
+@attr.s
+class TaskEventsCollection(BaseCollection):
+    ENTITY = TaskEvent
+
+    def get_all_event_ids(self):
+        view = self.application.web_ui.create_view(TaskEventsView)
+        time.sleep(2)
+        return view.all_event_ids
+
+    def get_events(self):
+        view = ViaWebUI.navigate_to(self.parent, "Events")
+        event_list = []
+        for event_id in self.get_all_event_ids():
+            event = self.instantiate(
+                event_id,
+                view.events(event_id).description.text,
+                view.events(event_id).date.text,
+                view.cluster_name.text,
+                view.events(event_id).event_type,
+                #TODO: include task_id
+                )
+            event_list.append(event)
+        return event_list
---
+++ usmqe_tests/ui/test_events_ui.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+@pytest.mark.author("ebondare@redhat.com")
+@pytest.mark.happypath
+@pytest.mark.testready
+def test_event_attributes(application):
+    """
+    Check that all common event attributes are as expected
+    """
+    """
+    :step:
+      Log in to Web UI and get the first cluster from the cluster list.
+      Get the list of events associated with this cluster.
+    :result:
+      Event objects are initiated and their attributes are read from Tasks page
+    """
+    clusters = application.collections.clusters.get_clusters()
+    test_cluster = clusters[0]
+    events = test_cluster.events.get_events()
+    """
+    :step:
+      Check that events list is not empty and that each event has description
+      of reasonable length and date within reasonable range.
+    :result:
+      Attributes of all events in the event list are as expected.
+    """
+
+    pytest.check(events != [])
+    for event in events:
+        pytest.check(len(event.description) > 15)
+        pytest.check(int(event.date.split(" ")[2]) > 2010)
+        pytest.check(int(event.date.split(" ")[2]) < 2100)
