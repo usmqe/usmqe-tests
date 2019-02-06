d1aabdcc91ab8fdaf1dea55b368b7a3f13d2aef2
Filip Balák <fbalak@redhat.com>
2019-02-06 16:16:17 +0100

    add hosts status tests

--- usmqe_tests/alerting/test_status_alerting.py
+++ usmqe_tests/alerting/test_status_alerting.py
@@ -174,3 +174,172 @@ def test_volume_status_api_alert(
             "There should be 1 alert:\nBody: '{0}'\n"\
             "There is {1}".format(
                 msg, alert_count))
+
+
+@pytest.mark.author("fbalak@redhat.com")
+@pytest.mark.ansible_playbook_teardown('test_teardown.stop_tendrl_nodes.yml')
+@pytest.mark.ansible_playbook_setup('test_setup.stop_tendrl_nodes.yml')
+@pytest.mark.ansible_playbook_setup('test_setup.smtp.yml')
+def test_host_status_mail_alert(
+        ansible_playbook,
+        workload_stop_hosts, default_entities):
+    """
+    Check that Tendrl sends correct status alert when host is stopped.
+    """
+    """
+    :step:
+      Get the messages that arrived in the interval provided by
+      workload_stopped_host fixture or a little later
+    :result:
+      The list of all relevant messages
+    """
+
+    alerting = Alerting("root")
+    entities = default_entities
+    severity = "WARNING"
+    for host in workload_stop_hosts["result"]:
+        entities["host"] = host
+
+        entities["value"] = "Disconnected"
+        mail_subject, mail_msg = alerting.generate_alert_msg(
+            domain="node",
+            subject="status",
+            entities=entities)
+        alert_count = alerting.search_mail(
+            "[{0}] {1}".format(severity, mail_subject),
+            mail_msg,
+            workload_stop_hosts['start'],
+            workload_stop_hosts['end'])
+        pytest.check(
+            alert_count == 1,
+            "There should be 1 alert:\nSubject: '{0}'\nBody: '{1}'\n"\
+            "There is {2}".format(
+                mail_subject, mail_msg, alert_count))
+
+    entities["value"] = "unhealthy"
+    mail_subject, mail_msg = alerting.generate_alert_msg(
+        domain="cluster",
+        subject="status",
+        entities=entities)
+    alert_count = alerting.search_mail(
+        "[{0}] {1}".format(severity, mail_subject),
+        mail_msg,
+        workload_stop_hosts['start'],
+        workload_stop_hosts['end'])
+    pytest.check(
+        alert_count == 1,
+        "There should be 1 alert:\nSubject: '{0}'\nBody: '{1}'\n"\
+        "There is {2}".format(
+            mail_subject, mail_msg, alert_count))
+
+
+@pytest.mark.author("fbalak@redhat.com")
+@pytest.mark.ansible_playbook_teardown('test_teardown.stop_tendrl_nodes.yml')
+@pytest.mark.ansible_playbook_setup('test_setup.stop_tendrl_nodes.yml')
+@pytest.mark.ansible_playbook_setup('test_setup.snmp.yml')
+def test_host_status_snmp_alert(
+        ansible_playbook,
+        workload_stop_hosts, default_entities):
+    """
+    Check that Tendrl sends correct status alert when host is stopped.
+    """
+    """
+    :step:
+      Get the messages that arrived in the interval provided by
+      workload_stopped_host fixture or a little later
+    :result:
+      The list of all relevant messages
+    """
+
+    alerting = Alerting("root")
+    entities = default_entities
+    severity = "WARNING"
+    for host in workload_stop_hosts["result"]:
+        entities["host"] = host
+
+        entities["value"] = "Disconnected"
+        mail_subject, mail_msg = alerting.generate_alert_msg(
+            domain="node",
+            subject="status",
+            entities=entities)
+        alert_count = alerting.search_snmp(
+            "[{0}], {1}-{2}".format(severity, mail_subject, mail_msg),
+            workload_stop_hosts['start'],
+            workload_stop_hosts['end'])
+        pytest.check(
+            alert_count == 1,
+            "There should be 1 alert:\nSubject: '{0}'\nBody: '{1}'\n"\
+            "There is {2}".format(
+                mail_subject, mail_msg, alert_count))
+
+    entities["value"] = "unhealthy"
+    mail_subject, mail_msg = alerting.generate_alert_msg(
+        domain="cluster",
+        subject="status",
+        entities=entities)
+    alert_count = alerting.search_snmp(
+        "[{0}], {1}-{2}".format(severity, mail_subject, mail_msg),
+        workload_stop_hosts['start'],
+        workload_stop_hosts['end'])
+    pytest.check(
+        alert_count == 1,
+        "There should be 1 alert:\nSubject: '{0}'\nBody: '{1}'\n"\
+        "There is {2}".format(
+            mail_subject, mail_msg, alert_count))
+
+
+@pytest.mark.author("fbalak@redhat.com")
+@pytest.mark.ansible_playbook_teardown('test_teardown.stop_tendrl_nodes.yml')
+@pytest.mark.ansible_playbook_setup('test_setup.stop_tendrl_nodes.yml')
+@pytest.mark.ansible_playbook_setup('test_setup.alerts_logger.yml')
+def test_host_status_api_alert(
+        ansible_playbook,
+        workload_stop_hosts, default_entities):
+    """
+    Check that Tendrl sends correct status alert when host is stopped.
+    """
+    """
+    :step:
+      Get the messages that arrived in the interval provided by
+      workload_stopped_host fixture or a little later
+    :result:
+      The list of all relevant messages
+    """
+
+    alerting = Alerting()
+    entities = default_entities
+    severity = "WARNING"
+    for host in workload_stop_hosts["result"]:
+        entities["host"] = host
+
+        entities["value"] = "Disconnected"
+        _, msg = alerting.generate_alert_msg(
+            domain="node",
+            subject="status",
+            entities=entities)
+        alert_count = alerting.search_api(
+            severity,
+            msg,
+            workload_stop_hosts['start'],
+            workload_stop_hosts['end'])
+        pytest.check(
+            alert_count == 1,
+            "There should be 1 alert:\nBody: '{0}'\n"\
+            "There is {1}".format(
+                msg, alert_count))
+
+    entities["value"] = "unhealthy"
+    _, msg = alerting.generate_alert_msg(
+        domain="cluster",
+        subject="status",
+        entities=entities)
+    alert_count = alerting.search_api(
+        severity,
+        msg,
+        workload_stop_hosts['start'],
+        workload_stop_hosts['end'])
+    pytest.check(
+        alert_count == 1,
+        "There should be 1 alert:\nBody: '{0}'\n"\
+        "There is {1}".format(
+            msg, alert_count))
